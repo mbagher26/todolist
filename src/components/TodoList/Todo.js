f658171f2d1dd45f64a1d2a56b50cea336e33bb7
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-29 08:02:02 -0400

    import and placement two icons for delete and check button

--- src/components/TodoList/Todo.js
+++ src/components/TodoList/Todo.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { FaCheck } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 
 export default class Todo extends Component {
 
@@ -9,11 +12,12 @@ export default class Todo extends Component {
                 <li className="todo-item">{this.props.title}</li>
 
                 <button className="check-btn">
-                    <i className="fas fa-check" aria-hidden="true"></i>
+                    <i className="fas fa-check" aria-hidden="true"><FaCheck />
+                    </i>
                 </button>
 
                 <button className="trash-btn">
-                    <i className="fas fa-trash" aria-hidden="true"></i>
+                    <i className="fas fa-trash" aria-hidden="true"><MdDelete/></i>
                 </button>
             </div>
         )
